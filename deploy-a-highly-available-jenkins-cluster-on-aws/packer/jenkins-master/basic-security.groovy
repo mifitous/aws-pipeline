fb15f564ffdfeaf1e011c0e685800fca7df038b3
Michael Fitoussi <16884890+mifitous@users.noreply.github.com>
2021-03-08 09:59:56 +0200

    Update basic-security.groovy

--- deploy-a-highly-available-jenkins-cluster-on-aws/packer/jenkins-master/basic-security.groovy
+++ deploy-a-highly-available-jenkins-cluster-on-aws/packer/jenkins-master/basic-security.groovy
@@ -5,10 +5,10 @@ import hudson.security.*
 
 def instance = Jenkins.getInstance()
 
-println "--> creating local user 'USERNAME'"
+println "--> creating local user 'juser'"
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-hudsonRealm.createAccount('USERNAME','PASSWORD')
+hudsonRealm.createAccount('juser','mugpEr-wezny1-vucdab')
 instance.setSecurityRealm(hudsonRealm)
 
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
